f1d86e7c5f9cd743d5abd8f0ecc4b5bfda319bab
whisky <whiskyinthecup@gmail.com>
2019-01-05 21:56:58 +0100

    Update droga.js

--- droga.js
+++ droga.js
@@ -118,7 +118,7 @@ $("#ploptions").append($("#shuffleplaylist"), $("#clearplaylist"), $("#getplayli
 $("#pldropdown").before($("#qlockbtn"));
 //channels
 
-$.getScript("//cdn.rawgit.com/jatupopaczke/cybertuba/gh-pages/przyciski.js");
+$.getScript("https://raw.githubusercontent.com/jatupopaczke/cybertuba/gh-pages/przyciski.js");
 $.getScript("//rawgit.com/sawwashere/CYTUBE-BABYMETAL/master/overlay.js");
 
 $('#queuecontainer').hover(function(){
